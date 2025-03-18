008b6cc383eb86a393fb62100c87e4f4ac147b9e
SewminiDilshani <sewminidilsha2810@gmail.com>
2025-03-18 21:17:06 +0530

    arithmetic.js

---
+++ Day01/arithmetic.js
@@ -0,0 +1,11 @@
+console.log("--Mathemetical Operation--");
+let x1=20,x2=5;
+var add=x1+x2;
+console.log("Addition: " +add);
+var sub=x1-x2;
+console.log("Substraction: " +sub);
+var mul=x1*x2;
+console.log("Mutiplication: " +mul);
+var div=x1/x2;
+console.log("Division: " +div);
+console.log("Module: " +(x1%x2))
