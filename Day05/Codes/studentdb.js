b80b4b72da13522991b72ad83f70eb6765d06b9a
SewminiDilshani <sewminidilsha2810@gmail.com>
2025-03-24 22:15:13 +0530

    Add files via upload

---
+++ Day05/Codes/studentdb.js
@@ -0,0 +1,11 @@
+//array of student JSON details
+let students = [
+    {regno:'2021ICT114',name:'Sewmini',age:23,course:'IT',gender:'female'},
+    {regno:'2021ICT2',name:'Gayan',age:22,course:'IT',gender:'male'},
+    {regno:'2021ICT3',name:'Kalani',age:20,course:'IT',gender:'female'},
+    {regno:'2021ICT116',name:'Nihal',age:21,course:'IT',gender:'male'},
+    {regno:'2021ICT5',name:'Sithum',age:22,course:'IT',gender:'male'},
+    {regno:'2021ICT7',name:'Amala',age:23,course:'IT',gender:'female'}
+    ];
+
+module.exports = students;
