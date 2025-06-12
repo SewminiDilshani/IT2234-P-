23268dfcd5fd3b3b8ff1b57b862b27350edfb2e3
SewminiDilshani <sewminidilsha2810@gmail.com>
2025-06-12 13:57:49 +0530

    Add files via upload

---
+++ Day19/Codes/userRoute.js
@@ -0,0 +1,60 @@
+const express=require('express')
+const router = express.Router()
+const User=require('../models/User')
+const { mongoose } = require('mongoose')
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
+const secretkey = "vau.phy@2025"
+
+router.post('/register',async(req,res)=>{
+    try{
+        let {username,email,password} = req.body
+        if(!username || !email || !password) {
+            res.status(400).send("Please provide data to the required fileds!")
+        }
+        if(await User.findOne({username})){
+            res.status(400).send("Username already available!")
+        }
+        const salt = await bcrypt.genSalt()
+        password = await bcrypt.hash(password,salt)
+        const results = await User.create({username,email,password})
+        return res.status(200).json(results)
+        
+    }catch(error){
+        console.error(error);
+        res.status(500).send("Server Error!")
+    }
+})
+
+router.post('/login',async(req,res)=>{
+    try{
+        let {username,email,password} = req.body
+        if(!username || !password) {
+            res.status(400).send("Please provide data to the required fileds!")
+        }
+
+        //check username
+        const user = await User.findOne({username})
+        if(!user){
+            res.status(409).send("Please check your username and password!")
+        }
+
+        //check password
+        const passMatch = await bcrypt.compare(password,user.password)
+        if(!passMatch){
+            res.status(409).send("Please check your username and password!")
+        }
+        
+        //create token
+        const payload = {ID:user._id,NAME:user.username}
+        //encryption
+        const token = jwt.sign(payload,secretkey)
+        return res.status(200).json({token,payload})
+        //return res.status(200).json(token)
+    }catch(error){
+        console.error(error);
+        res.status(500).send("Server Error!")
+    }
+})
+
+module.exports=router
